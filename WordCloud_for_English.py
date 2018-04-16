ad019bc4a3b8fbd74897be4d3030f4ca85bfe9d9
eptyee <35125627+eptyee@users.noreply.github.com>
2018-04-16 21:18:35 +0800

    Update WordCloud_for_English.py

--- WordCloud_for_English.py
+++ WordCloud_for_English.py
@@ -1,34 +1,32 @@
 
 # coding: utf-8
-
-# In[1]:
-
-
 from os import path
 from scipy.misc import imread
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS,  ImageColorGenerator
-# 获取当前文件路径
+
+# set the dir path
 dir = path.dirname('.')
-text = open(path.join(dir, 'English.txt')).read()#英文直接读取
-# 设置背景图片
+text = open(path.join(dir, 'English.txt')).read()# the words are saved in the text
+# set the color_mask
 mask_coloring = imread(path.join(dir, "th.png"))
-wc = WordCloud(background_color="white", #背景颜色
-                            max_words=1000,# 词云显示的最大词�?                            mask=mask_coloring,#设置背景图片                          
-                            max_font_size=80, #字体最大�?                            random_state=80)
-wc.generate(text) #分词
-image_colors = ImageColorGenerator(mask_coloring) # 从背景图片生成颜色�?# 显示图片
+wc = WordCloud(background_color="white",
+                            max_words=1000,                           mask=mask_coloring,                         
+                            max_font_size=80,                           random_state=80)
+#
+wc.generate(text) 
+image_colors = ImageColorGenerator(mask_coloring) # 
 plt.figure()
 plt.imshow(wc)
 plt.axis("off")
-# 绘制词云
+# 
 plt.figure()
 plt.imshow(wc.recolor(color_func=image_colors))  # recolor wordcloud and show
 plt.axis("off")
-# 绘制背景图片为颜色的图片
+#
 plt.figure()
 plt.imshow(mask_coloring, cmap=plt.cm.gray)
 plt.axis("off")
 plt.show()
-# 保存图片
+# save the files
 wc.to_file(path.join(dir, "wordcloud.png"))
